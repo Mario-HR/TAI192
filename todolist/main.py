09669bd483a6697fff31ce467193826378297875
Mario Rodarte <mariohrodarte@outlook.com>
2025-02-13 16:52:25 -0600

    Repaso Fast API - Endpoint para obtener todas las tareas

--- todolist/main.py
+++ todolist/main.py
@@ -6,3 +6,18 @@ app=FastAPI(
     description='Mario Alberto Hernandez Rodarte',
     version='1.0'
 )
+
+tareas=[
+    {
+        "id": 1,
+        "titulo": "Estudiar para el examen",
+        "descripcion": "Repasar los apuntes de TAI ",
+        "vencimiento": "14-02-24",
+        "estado": "completada"
+    }
+]
+
+#Endpoint para obtener todas las tareas
+@app.get('/tareas', tags=['Consulta de tareas'])
+def leer_tareas():
+    return {"Tareas": tareas}
